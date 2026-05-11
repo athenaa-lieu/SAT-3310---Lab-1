224b8c015a3406c950e02d633278ec6f201a63b5
Athena Lieu <athenaaabelle@gmail.com>
2026-05-10 22:27:38 -0400

    comments

--- sat3310-s26-lab01-linux.pl
+++ sat3310-s26-lab01-linux.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
+#
+# The traditional first program in any language.
 
+# Strict and warnings are recommended.
 use strict;
 use warnings;
 
+# Print a message.
 print "Hello, World!\n";
