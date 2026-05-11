f06f67bd5473cd3ff9e260bf1c6bf4ada386e115
Athena Lieu <athenaaabelle@gmail.com>
2026-05-11 00:12:57 -0400

    powershell

--- sat3310-s26-lab01.ps1
+++ sat3310-s26-lab01.ps1
@@ -1,3 +1,9 @@
 write-host "Hello, World!"
 
-Set-ExecutionPolicy -Scope Process RemoteSigned
+# Set-ExecutionPolicy -Scope Process RemoteSigned
+$scriptPath = "C:\path\to\your\script.ps1"
+if (Test-Path $scriptPath) {
+    . $scriptPath
+} else {
+    write-host "Script not found at $scriptPath"
+}
